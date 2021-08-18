1dbd8eef42a4fdd3b8425696c78c47cd281c3437
cozuf <coskunyusuf43@gmail.com>
2021-08-19 00:12:21 +0300

    PageContainer düzenlendi

--- src/Components/PageContainer/index.tsx
+++ src/Components/PageContainer/index.tsx
@@ -6,6 +6,7 @@ import {
   ViewStyle,
   StyleSheet,
   useColorScheme,
+  Omit,
 } from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import {SafeAreaView} from 'react-native-safe-area-context';
@@ -13,51 +14,71 @@ import {TOKENS} from '../../Theme';
 import {dark, light} from '../../Theme/Variants';
 
 interface IPageContainerProps {
+  /**
+   *
+   */
   type: 'SafeArea' | 'Default' | 'Scroll';
+
+  /**
+   *
+   */
   style?: ViewStyle;
+
+  /**
+   *
+   */
   contentContainerStyle?: ViewStyle;
 }
 
-const PageContainer: FC<IPageContainerProps & (ViewProps | ScrollViewProps)> =
-  ({type = 'Default', style, contentContainerStyle, children, ...props}) => {
-    const isDarkMode = useColorScheme() === 'dark';
-    const COLOR = isDarkMode
-      ? dark.pageContainer?.background
-      : light.pageContainer?.background;
-    switch (type) {
-      case 'SafeArea':
-        return (
-          <SafeAreaView
-            style={[styles.view, {backgroundColor: COLOR}, style]}
-            {...props}>
-            {children}
-          </SafeAreaView>
-        );
+type IPageContainerTypes = IPageContainerProps &
+  (
+    | Omit<ViewProps, 'style'>
+    | Omit<ScrollViewProps, 'style' | 'contentContainerStyle'>
+  );
 
-      case 'Scroll':
-        return (
-          <ScrollView
-            style={[style]}
-            contentContainerStyle={[
-              styles.scrollview,
-              {backgroundColor: COLOR},
-              contentContainerStyle,
-            ]}
-            {...props}>
-            {children}
-          </ScrollView>
-        );
-      case 'Default':
-      default:
-        return (
-          <View
-            style={[styles.view, {backgroundColor: COLOR}, style]}
-            {...props}>
-            {children}
-          </View>
-        );
-    }
-  };
+const PageContainer: FC<IPageContainerTypes> = ({
+  type = 'Default',
+  style,
+  contentContainerStyle,
+  children,
+  ...props
+}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const COLOR = isDarkMode
+    ? dark.pageContainer?.background
+    : light.pageContainer?.background;
+  switch (type) {
+    case 'SafeArea':
+      return (
+        <SafeAreaView
+          style={[styles.view, {backgroundColor: COLOR}, style]}
+          {...props}>
+          {children}
+        </SafeAreaView>
+      );
+
+    case 'Scroll':
+      return (
+        <ScrollView
+          style={[style]}
+          contentContainerStyle={[
+            styles.scrollview,
+            {backgroundColor: COLOR},
+            contentContainerStyle,
+          ]}
+          {...props}>
+          {children}
+        </ScrollView>
+      );
+    case 'Default':
+    default:
+      return (
+        <View style={[styles.view, {backgroundColor: COLOR}, style]} {...props}>
+          {children}
+        </View>
+      );
+  }
+};
 
 export default PageContainer;
 
