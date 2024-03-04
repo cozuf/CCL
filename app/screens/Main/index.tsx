95e21fc09ab27ad6926b6d5e6758e1e7b2ac851b
cozuf <coskunyusuf43@gmail.com>
2024-03-05 02:27:21 +0300

    main page düzenlendi

--- app/screens/Main/index.tsx
+++ app/screens/Main/index.tsx
@@ -1,8 +1,8 @@
 import React from "react"
-import { Button, FlatList, View } from "react-native"
+import { FlatList } from "react-native"
 import { NavigationProp, ParamListBase, useNavigation } from "@react-navigation/native"
 import COMPONENT_LIST from "../../../src/components/list"
-import { PageContainer, Text } from "../../../src/components"
+import { Button, PageContainer, Separator } from "../../../src"
 
 const Main = () => {
     const navigation = useNavigation<NavigationProp<ParamListBase>>()
@@ -12,18 +12,24 @@ const Main = () => {
         path: v.toUpperCase()
     }))
 
+    const onPressComponent = (path: string) => {
+        navigation.navigate(path)
+    }
+
     return (
         <PageContainer>
-            <Button onPress={() => { }} title="Yusuf" />
             <FlatList
+                bounces={false}
                 data={items}
                 renderItem={({ item, index }) => {
                     return (
-                        <Text onPress={() => navigation.navigate(item.path)}>
-                            {item.label} - {item.path}
-                        </Text>
+                        <Button
+                            title={` ${item.label} - ${item.path}`}
+                            onPress={() => onPressComponent(item.path)}
+                        />
                     )
                 }}
+                ItemSeparatorComponent={() => <Separator />}
             />
         </PageContainer>
     )
