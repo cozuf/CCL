bf5b317d308de2025bd8e5f5991068d2a3340551
cozuf <coskunyusuf43@gmail.com>
2024-03-04 08:15:23 +0300

    component listesi tanımlandı

---
+++ src/components/list.ts
@@ -0,0 +1,20 @@
+const COMPONENT_LIST = [
+    "Badge",
+    "Button",
+    "Card",
+    "CheckBox",
+    "CheckBoxGroup",
+    "Chip",
+    "ChipGroup",
+    "PageContainer",
+    "RadioButton",
+    "RadioButtonGroup",
+    "SegmentedButton",
+    "SelectBox",
+    "Separator",
+    "TapSelector",
+    "Text",
+    "TextInput"
+]
+
+export default COMPONENT_LIST
