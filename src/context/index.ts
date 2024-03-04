5b63fd0d0186e6865d312a0df97af2598adbab69
cozuf <coskunyusuf43@gmail.com>
2024-03-04 06:21:53 +0300

    context tanımlandı

---
+++ src/context/index.ts
@@ -0,0 +1,7 @@
+import { ThemeProvider, useTheme, useSetTheme, withTheme } from './theme';
+import { CCLProvider } from "./ccl"
+
+export {
+    ThemeProvider, useTheme, useSetTheme, withTheme,
+    CCLProvider
+}
