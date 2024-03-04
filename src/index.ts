f9aa9bfb62cd3fdf19617a46ca4f5fea98099505
cozuf <coskunyusuf43@gmail.com>
2024-03-04 08:05:26 +0300

    exportlar tanımlandı

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export type { IPageContainerProps, ISeparatorProps, ITextProps } from "./components"
+
+export { PageContainer, Separator, Text } from "./components"
+
+export {
+    ThemeProvider, useTheme, useSetTheme, withTheme,
+    CCLProvider
+} from "./context"
+
+export { fonts, tokens, dark as darkColors, light as lightColors } from "./theme"
