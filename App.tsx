3f373d9ded9c54f90c2aef9b133c26906c214947
cozuf <coskunyusuf43@gmail.com>
2024-03-04 08:05:45 +0300

    App.tsx güncellendi

--- App.tsx
+++ App.tsx
@@ -1,118 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect } from 'react';
+import { SafeAreaView, StatusBar, useColorScheme, NativeModules } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { DarkTheme, DefaultTheme, NavigationContainer, Theme } from '@react-navigation/native';
+import Router from './app/screens';
+import { CCLProvider, darkColors, lightColors, tokens, fonts } from './src';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const { StatusBarManager } = NativeModules
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
+const App = (): JSX.Element => {
   const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // const NavigationTheme: Theme = {
+  //   dark: isDarkMode,
+  //   colors: {
+  //     primary: isDarkMode ? 'rgb(0, 210, 170)' : 'rgb(255, 45, 85)',
+  //     background: isDarkMode ? 'rgb(13,13,13)' : 'rgb(242, 242, 242)',
+  //     card: isDarkMode ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)',
+  //     text: isDarkMode ? 'rgb(227, 227, 225)' : 'rgb(28, 28, 30)',
+  //     border: 'rgb(199, 199, 204)',
+  //     notification: 'rgb(255, 69, 58)',
+  //   },
+  // };
 
   const backgroundStyle = {
+    flex: 1,
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  useEffect(() => {
+    StatusBarManager.getHeight((statusBarHeight: any) => {
+      // console.warn(statusBarHeight)
+    })
+  }, [])
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <CCLProvider
+      theme={{
+        name: isDarkMode ? "dark" : "light",
+        colors: isDarkMode ? darkColors : lightColors,
+        fonts,
+        tokens
+      }}>
+      <NavigationContainer
+        theme={isDarkMode ? DarkTheme : DefaultTheme}>
+        {/* <SafeAreaView style={backgroundStyle}> */}
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <Router />
+        {/* </SafeAreaView> */}
+      </NavigationContainer>
+    </CCLProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
