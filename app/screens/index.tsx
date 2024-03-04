56459688f73964747a0a4e73c728a3b3d9e7cbfa
cozuf <coskunyusuf43@gmail.com>
2024-03-04 08:15:06 +0300

    ekranlar tanımlandı

---
+++ app/screens/index.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import PAGE_NAMES from "./pageNames"
+import PAGES from "./pages"
+import { useTheme } from "../../src/context"
+
+const Stack = createNativeStackNavigator()
+
+const Router = () => {
+    const { fonts } = useTheme()
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerTitleStyle: { fontFamily: fonts.bold },
+                headerBackTitleStyle: { fontFamily: fonts.semibold }
+            }}>
+            <Stack.Screen name={PAGE_NAMES.MAIN} component={PAGES.Main} />
+            <Stack.Screen name={PAGE_NAMES.BADGE} component={PAGES.Badge} />
+            <Stack.Screen name={PAGE_NAMES.BUTTON} component={PAGES.Button} />
+            <Stack.Screen name={PAGE_NAMES.CARD} component={PAGES.Card} />
+            <Stack.Screen name={PAGE_NAMES.CHECKBOX} component={PAGES.CheckBox} />
+            <Stack.Screen name={PAGE_NAMES.CHECKBOXGROUP} component={PAGES.CheckBoxGroup} />
+            <Stack.Screen name={PAGE_NAMES.CHIP} component={PAGES.Chip} />
+            <Stack.Screen name={PAGE_NAMES.CHIPGROUP} component={PAGES.ChipGroup} />
+            <Stack.Screen name={PAGE_NAMES.PAGECONTAINER} component={PAGES.PageContainer} />
+            <Stack.Screen name={PAGE_NAMES.RADIOBUTTON} component={PAGES.RadioButton} />
+            <Stack.Screen name={PAGE_NAMES.RADIOBUTTONGROUP} component={PAGES.RadioButtonGroup} />
+            <Stack.Screen name={PAGE_NAMES.SEGMENTEDBUTTON} component={PAGES.SegmentedButton} />
+            <Stack.Screen name={PAGE_NAMES.SELECTBOX} component={PAGES.SelectBox} />
+            <Stack.Screen name={PAGE_NAMES.SEPARATOR} component={PAGES.Separator} />
+            <Stack.Screen name={PAGE_NAMES.TAPSELECTOR} component={PAGES.TapSelector} />
+            <Stack.Screen name={PAGE_NAMES.TEXT} component={PAGES.Text} />
+            <Stack.Screen name={PAGE_NAMES.TEXTINPUT} component={PAGES.TextInput} />
+        </Stack.Navigator>
+    )
+}
+
+export default Router
---
+++ app/screens/pages.ts
@@ -0,0 +1,36 @@
+import Main from "./Main";
+import Badge from "./Badge";
+import Button from "./Button";
+import Card from "./Card";
+import CheckBox from "./CheckBox";
+import CheckBoxGroup from "./CheckBoxGroup";
+import Chip from "./Chip";
+import ChipGroup from "./ChipGroup";
+import PageContainer from "./PageContainer";
+import RadioButton from "./RadioButton";
+import RadioButtonGroup from "./RadioButtonGroup";
+import SegmentedButton from "./SegmentedButton";
+import SelectBox from "./SelectBox";
+import Separator from "./Separator";
+import TapSelector from "./TapSelector";
+import Text from "./Text";
+import TextInput from "./TextInput";
+
+const PAGES = {
+    Main,
+    Badge,
+    Button,
+    Card,
+    CheckBox, CheckBoxGroup,
+    Chip, ChipGroup,
+    PageContainer,
+    RadioButton, RadioButtonGroup,
+    SegmentedButton,
+    SelectBox,
+    Separator,
+    TapSelector,
+    Text,
+    TextInput
+}
+
+export default PAGES
