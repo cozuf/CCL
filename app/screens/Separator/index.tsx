816a9555acdd16366743bfba2509dc9679b85d08
cozuf <coskunyusuf43@gmail.com>
2024-03-07 15:53:07 +0300

    Separator örneği düzenlendi

--- app/screens/Separator/index.tsx
+++ app/screens/Separator/index.tsx
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
-import { PageContainer, Separator } from "../../../src";
+import { PageContainer, Separator, TapSelector } from "../../../src";
 
+const DIRECTION = [
+    {
+        title: "Vertical",
+        value: "vertical"
+    },
+    {
+        title: "Horizontal",
+        value: "horizontal"
+    }
+]
+const DISTANCE = [
+    {
+        title: "4",
+        value: 4
+    },
+    {
+        title: "default 8",
+        value: undefined
+    },
+    {
+        title: "16",
+        value: 16
+    },
+    {
+        title: "32",
+        value: 32
+    }
+]
 const SeparatorPage = () => {
+    const [directionIndex, setDirectionIndex] = useState<number>(0)
+    const [distanceIndex, setDistanceIndex] = useState<number>(0)
+
+    const isVertical = DIRECTION[directionIndex].value === "vertical"
+
     return (
         <PageContainer>
-            <View style={{ height: 40, backgroundColor: "red" }} />
+            <View style={{ height: 200, flexDirection: isVertical ? "column" : "row" }}>
+                <View style={[isVertical ? { height: 40, } : { width: 40 }, { backgroundColor: "red" }]} />
+
+                <Separator
+                    direction={isVertical ? "vertical" : "horizontal"}
+                    distance={DISTANCE[distanceIndex].value}
+                />
+                <View style={[isVertical ? { height: 40, } : { width: 40 }, { backgroundColor: "blue" }]} />
+            </View>
             <Separator distance={80} />
-            <View style={{ height: 40, backgroundColor: "blue" }} />
+            <View>
+                <TapSelector
+                    initialIndex={directionIndex}
+                    data={DIRECTION}
+                    onTap={(v, i) => { setDirectionIndex(i) }}
+                />
+                <Separator />
+                <TapSelector
+                    initialIndex={distanceIndex}
+                    data={DISTANCE}
+                    onTap={(v, i) => { setDistanceIndex(i) }}
+                />
+            </View>
         </PageContainer>
     )
 }
