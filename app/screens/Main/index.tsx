5d2ddd973b886a14f240b7f877e752f4d1100745
cozuf <coskunyusuf43@gmail.com>
2024-03-08 03:09:59 +0300

    tema değiştirme eklendi

--- app/screens/Main/index.tsx
+++ app/screens/Main/index.tsx
@@ -1,10 +1,13 @@
 import React from "react"
-import { FlatList } from "react-native"
+import { FlatList, View } from "react-native"
 import { NavigationProp, ParamListBase, useNavigation } from "@react-navigation/native"
 import COMPONENT_LIST from "../../../src/components/list"
-import { Button, PageContainer, Separator } from "../../../src"
+import { Button, PageContainer, Separator, darkColors, useSetTheme, useTheme } from "../../../src"
+import { light } from "../../../src/theme"
 
 const Main = () => {
+    const { name } = useTheme()
+    const setTheme = useSetTheme()
     const navigation = useNavigation<NavigationProp<ParamListBase>>()
 
     const items = COMPONENT_LIST.map((v) => ({
@@ -17,11 +20,27 @@ const Main = () => {
         navigation.navigate(path)
     }
 
+    const renderHeader = () => {
+        return (
+            <View style={{ paddingVertical: 16 }}>
+                <Button
+                    type="simplified"
+                    title={`Tema - ${name} - Değiştir`}
+                    onPress={() => {
+
+                        setTheme({ name: name === "light" ? "dark" : "light", colors: name === "light" ? darkColors : light })
+                    }}
+                />
+            </View>
+        )
+    }
+
     return (
         <PageContainer>
             <FlatList
                 bounces={false}
                 data={items}
+                ListHeaderComponent={renderHeader}
                 renderItem={({ item, index }) => {
                     return (
                         <Button
