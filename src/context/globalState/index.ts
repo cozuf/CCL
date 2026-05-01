31d88453c0d67815fb3eb3c93656507233dfd59e
cozuf <coskunyusuf43@gmail.com>
2026-05-01 07:49:35 +0300

    naming fixed

--- src/context/globalState/index.ts
+++ src/context/globalState/index.ts
@@ -1,4 +1,4 @@
-import GlobalStateProvider, { IGLobalStateRef } from "./proivder";
+import GlobalStateProvider, { IGLobalStateRef } from "./provider";
 import { useGlobalState, useSetGlobalState } from "./hook";
 import { withGlobalState } from "./with"
 
